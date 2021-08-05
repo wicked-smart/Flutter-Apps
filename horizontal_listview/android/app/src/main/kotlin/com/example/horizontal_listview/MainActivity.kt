7829afb1aa7bbed59fa1e11a35adb0d1669e28cf
wicked-smart <marzaanvan@gmail.com>
2021-08-05 11:32:01 +0530

    Modify some files

---
+++ horizontal_listview/android/app/src/main/kotlin/com/example/horizontal_listview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.horizontal_listview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,115 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-            _saved.forEach((index) => print("$index"));
-            print("\n");
-          } else {
-            _saved.add(pair);
-            _saved.forEach((index) => print("$index"));
-            print("\n");
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        // NEW lines from here...
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-            (WordPair pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
-              : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        }, // ...to here.
-      ),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return Divider();
-          }
-
-          final int index = i ~/ 2;
-
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    //final wordpair = WordPair.random();
-    //return Text(wordpair.asPascalCase);
-
-    return Scaffold(
-      appBar: AppBar(
-          title: Text('start up name generator'),
-          actions: [IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))]),
-      body: _buildSuggestions(),
-    );
-  }
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final title = 'Horizontal List';
+
     return MaterialApp(
-      title: 'Welcome to flutter',
-      home: RandomWords(),
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(20.0),
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
